f1df1ea07cf5f6b4535d7fed2c506b1b96b13fdd
Pankaj <pkandpal601@gmail.com>
2022-09-30 13:12:02 +0530

    done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,13 +1,17 @@
 const urlModel =  require('../models/urlModel')
 const shortUrl = require('node-url-shortener');
 const axios = require('axios');
-const shortId = require('shortid')
+const shortId = require('shortid');
+const { findOne } = require('../models/urlModel');
 
 const createUrl = async function(req, res){
     try{
         let urlCreate = req.body
+        if(Object.keys(urlCreate).length == 0){
+            res.status(400).send({status: false, msg: 'Request body can not be empty'})
+        }
         if(!urlCreate.longUrl){
-            return res.status(400).send({status: false, msg: 'Please enter longUrl'});
+            return res.status(400).send({status: false, msg: 'Please enter longUrl Key'});
         }
         let correctLink = false 
         await axios.get(urlCreate.longUrl)
@@ -17,14 +21,18 @@ const createUrl = async function(req, res){
          if(correctLink == false){
           return  res.status(400).send({status : false, message : "Please Provide correct url!!"})
          } 
-         
+         let urlCheck = await urlModel.findOne({longUrl:urlCreate.longUrl})
+    
+         if(urlCheck){
+            return res.status(400).send({status: false, msg: 'shortUrl for this longUrl has already been generated'});
+         }
          let generateId = shortId.generate(urlCreate.longUrl)
          let shortUrl = `http://localhost:3000/${generateId}`
          
         let obj = {}
         obj.longUrl = req.body.longUrl
-        obj.shortUrl = req.createUrl
-        obj.urlCode = req.generateId;
+        obj.shortUrl = shortUrl
+        obj.urlCode = generateId;
         let saveUrl = await urlModel.create(obj);
         let saveUrl2 = await urlModel.findOne({_id: saveUrl._id}).select({_id: 0,longUrl:1, shortUrl: 1, urlCode: 1});
         return res.status(201).send({status: true, data:saveUrl2});
