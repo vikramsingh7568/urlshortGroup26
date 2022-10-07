82e61802ec46b3ae216667e56081dd5b4f6adf33
vikramsingh7568 <110588921+vikramsingh7568@users.noreply.github.com>
2022-10-07 21:30:37 +0530

    done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,6 @@
-const urlModel = require("../models/urlModel");
-const axios = require("axios");
-const shortId = require("shortid");
+const urlModel = require('../models/urlModel')
+const axios = require('axios');
+const shortId = require('shortid');
 const redis = require("redis");
 
 const { promisify } = require("util");
@@ -9,7 +9,7 @@ const { promisify } = require("util");
 const redisClient = redis.createClient(
   11693,
   "redis-11693.c212.ap-south-1-1.ec2.cloud.redislabs.com",
-  { no_ready_check: true } //optional
+  { no_ready_check: true }
 );
 
 redisClient.auth("fgY1Wek4Oag9aUeXDklbSzXyPp1As9VC", function (err) {
@@ -20,121 +20,123 @@ redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
 
+
+
+//1. connect to the server
+//2. use the commands :
+
 //Connection setup for redis
+
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+// await SET_ASYNC.connect();
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+// await GET_ASYNC.connect();
+
+
 
 const createUrl = async function (req, res) {
-  try {
-    let urlCreate = req.body;
+ try {
+  
+    let urlCreate = req.body
     if (Object.keys(urlCreate).length == 0) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Request body can not be empty" });
+      return res.status(400).send({ status: false, msg: 'Request body can not be empty' })
     }
     if (!urlCreate.longUrl) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please enter longUrl Key" });
+      return res.status(400).send({ status: false, msg: 'Please enter longUrl Key' });
     }
-    let correctLink = false;
-    await axios
-      .get(urlCreate.longUrl)
-      .then((res) => {
-        correctLink = true;
-      })
-      .catch((error) => {
-        correctLink = false;
-      });
+
+    let correctLink = false
+    await axios.get(urlCreate.longUrl)
+      .then((res) => { correctLink = true })
+      .catch((error) => { correctLink = false })
 
     if (correctLink == false) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please Provide correct url!!" });
+      return res.status(400).send({ status: false, message: "Please Provide correct url!!" })
     }
+     
+    
+  
 
-    let urlCheck = await urlModel
-      .findOne({ longUrl: urlCreate.longUrl })
-      .select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
-    //Getting data from caching server
-    let cacheProfileData = await GET_ASYNC(`${urlCreate.longUrl}`);
-    if (cacheProfileData) {
-      console.log("cache generated for this link");
-      let data = JSON.parse(cacheProfileData);
+    let cashProfileData = await GET_ASYNC(`${urlCreate.longUrl}`)
+    if(cashProfileData) {
+      console.log("cache generated for this link")
+      let data = JSON.parse(cashProfileData)
+
       let obj = {
-        shortUrl: data.shortUrl,
-        longUrl: data.longUrl,
-        urlCode: data.urlCode,
-      };
+       longUrl :  data.longurl,
+       shortUrl : data.shortUrl,
+       urlCode : data.urlCode
+      }
       return res.status(400).send({
-        status: false,
-        msg: "shortUrl for this longUrl has already been generated response form caching server",
-        data: obj,
+        status: false, msg: 'shortUrl for this longUrl has already been generated cashing',
+        data: obj
       });
-    }
-    //The code between 60- 75 lines used to get data from caching server
+    } 
+
+    let urlCheck = await urlModel.findOne({ longUrl: urlCreate.longUrl }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
     if (urlCheck) {
       return res.status(400).send({
-        status: false,
-        msg: "shortUrl for this longUrl has already been generated response from mongo db",
-        data: urlCheck,
+        status: false, msg: 'shortUrl for this longUrl has already been generated db',
+        data: urlCheck
       });
-    }
+   }
+   
+    let generateId = shortId.generate(urlCreate.longUrl)
+    let shortUrl = `http://localhost:3000/${generateId}`
 
-    let generateId = shortId.generate(urlCreate.longUrl);
-    let shortUrl = `http://localhost:3000/${generateId}`;
 
-    let obj = {};
-    obj.longUrl = req.body.longUrl;
-    obj.shortUrl = shortUrl;
+    let obj = {}
+    obj.longUrl = req.body.longUrl
+    obj.shortUrl = shortUrl
     obj.urlCode = generateId;
-
+    
+   
+ 
     let saveUrl = await urlModel.create(obj);
+     
+    await SET_ASYNC(`${saveUrl.longUrl}`, JSON.stringify(saveUrl),"EX", 1000)
 
-    await SET_ASYNC(`${saveUrl.longUrl}`, JSON.stringify(saveUrl)); //key(longUrl) => value(saveUrl)
+   
 
-    let saveUrl2 = await urlModel
-      .findOne({ _id: saveUrl._id })
-      .select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
-    return res
-      .status(201)
-      .send({ status: true, message: "created successfully", data: saveUrl2 });
+    let saveUrl2 = await urlModel.findOne({ _id: saveUrl._id }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
+    return res.status(201).send({ status: true,message : "created successfully", data: saveUrl2 });
+   
+
   } catch (err) {
-    res.status(500).send({ status: false, msg: err.message });
+   res.status(500).send({ status: false, msg: err.message })
   }
-};
 
-//second get api url
+}
+
+//second get api url 
 const getUrl = async function (req, res) {
   try {
-    let shortId = req.params.urlCode;
+    let shortId = req.params.urlCode
     if (!shortId) {
-      return res
-        .status(400)
-        .send({ status: false, msg: "Please enter valid urlCode" });
+      return res.status(400).send({ status: false, msg: 'Please enter valid urlCode' })
     }
 
-    // cashing part implemented here in this code
+    // cashing part implemented here in this code 
 
-    let cacheProfileData = await GET_ASYNC(`${shortId}`);
-    if (cacheProfileData) {
-      console.log("cache generated for this link");
-      let data = JSON.parse(cacheProfileData);
-      res.status(302).redirect(data.longUrl);
+    let cashProfileData = await GET_ASYNC(`${shortId}`)
+    if (cashProfileData) {
+      console.log("cache generated for this link")
+      let data = JSON.parse(cashProfileData)
+      res.status(302).redirect(data.longUrl)
     } else {
-      let getData = await urlModel.findOne({ urlCode: shortId });
+      let getData = await urlModel.findOne({ urlCode: shortId })
       if (!getData) {
-        return res.status(404).send({ status: false, msg: "no url found" });
+        return res.status(404).send({ status: false, msg: "no url found" })
       }
-      await SET_ASYNC(`${getData.urlCode}`, JSON.stringify(getData));
-      return res.status(302).redirect(getData.longUrl);
+      await SET_ASYNC(`${getData.urlCode}`, JSON.stringify(getData),"EX", 1000)
+      return res.status(302).redirect(getData.longUrl)
 
-      // all caching part in this part
+      // all cashing part in this part  
     }
   } catch (err) {
-    return res.status(500).send({ status: false, msg: err.message });
+    return res.status(500).send({ status: false, msg: err.message })
   }
-};
+}
 
-module.exports.createUrl = createUrl;
-module.exports.getUrl = getUrl;
+module.exports.createUrl = createUrl
+module.exports.getUrl = getUrl
